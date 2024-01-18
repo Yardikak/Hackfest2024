65ce2ff3a35eaf11dfa3f313029447cc4685909d
Yanwar Ardika C <yanwarardikacahyani77@gmail.com>
2024-01-19 02:25:18 +0800

    Commit pertama di waktu shubuh

---
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+//Pengerjaan sendiri team hacker yang lain entah kemana
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Selamat Datang Pak $name!",
+            modifier = modifier
+        )
+}
+
+//@Composable
+//fun GreetingText(message: String, modifier: Modifier = Modifier){
+//    Text(
+//        text = message
+//    )
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme{
+        Greeting("Dika!")
+    }
+}
